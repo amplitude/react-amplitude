905ef1d4f85eba181cff143ceb3efed9ade6238f
William Holloway <man.thursday@gmail.com>
2019-02-26 03:30:00 -0800

    Add tests (and fixes)

--- src/components/Amplitude.js
+++ src/components/Amplitude.js
@@ -1,8 +1,26 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
+import { AmplitudeContext } from './AmplitudeProvider';
 import { useInstrument } from '../hooks/use-instrument';
 import { useLogEvent } from '../hooks/use-log-event';
 import { useSetUserProperties } from '../hooks/use-set-user-properties';
+
+const useMergedContext = eventProperties => {
+  const context = React.useContext(AmplitudeContext);
+  const { getAmplitudeEventProperties } = context;
+  const newGetAmplitudeEventProperties = React.useCallback(() => {
+    return { ...getAmplitudeEventProperties(), ...(eventProperties || {}) };
+  });
+
+  return React.useMemo(
+    () => ({
+      ...context,
+      getAmplitudeEventProperties: newGetAmplitudeEventProperties,
+    }),
+    [context, newGetAmplitudeEventProperties],
+  );
+};
 
 const Amplitude = ({
   children,
@@ -13,13 +31,20 @@ const Amplitude = ({
 }) => {
   const logEvent = useLogEvent(instanceName, eventProperties, debounceInterval);
   const instrument = useInstrument(instanceName, eventProperties, debounceInterval);
+  const mergedContext = useMergedContext(eventProperties);
   useSetUserProperties(instanceName, userProperties);
 
+  let childElements;
+
   if (typeof children === 'function') {
-    return children({ instrument, logEvent });
+    childElements = children({ instrument, logEvent });
   } else {
-    return children || null;
+    childElements = children || null;
   }
+
+  return (
+    <AmplitudeContext.Provider value={mergedContext}>{childElements}</AmplitudeContext.Provider>
+  );
 };
 
 Amplitude.propTypes = {
